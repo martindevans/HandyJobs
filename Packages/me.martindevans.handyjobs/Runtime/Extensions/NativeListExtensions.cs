0ef3f54bab43261c9792ebe6439be914bda0fcaa
Martin Evans <martindevans@gmail.com>
2026-08-20 22:11:36 +0100

    Added `DrainTo` extension to queue

---
+++ Packages/me.martindevans.handyjobs/Runtime/Extensions/NativeListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Collections;
+
+namespace me.martindevans.handyjobs.Extensions
+{
+    public static class NativeListExtensions
+    {
+        public static void EnsureCapacity<T>(this NativeList<T> list, int capacity)
+            where T : unmanaged
+        {
+            if (capacity > list.Capacity)
+                list.SetCapacity(capacity);
+        }
+    }
+}
---
+++ Packages/me.martindevans.handyjobs/Tests/Collections/JobDrainQueueExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using me.martindevans.handyjobs.Primitives.Collections;
+using NUnit.Framework;
+using Unity.Collections;
+
+namespace Tests.Collections
+{
+    public class JobDrainQueueExtensionsTests
+    {
+        [Test]
+        public void DrainTo_AppendsQueuedItemsInOrder_AndHandlesEmptyQueue()
+        {
+            var queue = new NativeQueue<int>(Allocator.TempJob);
+            var list = new NativeList<int>(Allocator.TempJob) { 100, 200 };
+            try
+            {
+                queue.Enqueue(1);
+                queue.Enqueue(2);
+                queue.Enqueue(3);
+
+                queue.DrainTo(list, default).Complete();
+
+                Assert.AreEqual(5, list.Length);
+                Assert.AreEqual(100, list[0]);
+                Assert.AreEqual(200, list[1]);
+                Assert.AreEqual(1, list[2]);
+                Assert.AreEqual(2, list[3]);
+                Assert.AreEqual(3, list[4]);
+                Assert.AreEqual(0, queue.Count);
+
+                queue.DrainTo(list, default).Complete();
+
+                Assert.AreEqual(5, list.Length);
+                Assert.AreEqual(0, queue.Count);
+            }
+            finally
+            {
+                list.Dispose();
+                queue.Dispose();
+            }
+        }
+    }
+}
